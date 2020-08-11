0d4ed2474eb65244543c13a145e84ad866446b3e
Andrew Davis <pulpdrew@gmail.com>
2020-08-11 17:55:18 -0400

    add TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home: React.FC<any> = (x) =>  {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,3 +17,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;
