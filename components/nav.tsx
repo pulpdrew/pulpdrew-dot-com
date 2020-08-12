30a0809668f43d9587a4d7f8ea5e3c3c9ba12713
Andrew Davis <pulpdrew@gmail.com>
2020-08-12 14:17:17 -0400

    add placeholder tech and books pages

--- components/nav.tsx
+++ components/nav.tsx
@@ -18,8 +18,8 @@ const Nav: React.FC<{selected: NavItem}> = ({selected}) => {
     <nav className="sm:container flex flex-wrap mx-auto justify-between my-10">
       <Link href="/"><a className={selected === NavItem.MAIN ? selectedClasses : defaultClasses}>Pulpdrew</a></Link>
       <Link href="/about"><a className={selected === NavItem.ABOUT ? selectedClasses : defaultClasses}>About</a></Link>
-      <Link href=""><a className={selected === NavItem.TECH ? selectedClasses : defaultClasses}>Tech</a></Link>
-      <Link href=""><a className={selected === NavItem.BOOKS ? selectedClasses : defaultClasses}>Books</a></Link>
+      <Link href="/tech"><a className={selected === NavItem.TECH ? selectedClasses : defaultClasses}>Tech</a></Link>
+      <Link href="/books"><a className={selected === NavItem.BOOKS ? selectedClasses : defaultClasses}>Books</a></Link>
       <Link href="https://www.github.com/pulpdrew">
         <a>
           <img alt="GitHub" src="/github.png" className="w-8 h-8 mx-1 mb-1"></img>
