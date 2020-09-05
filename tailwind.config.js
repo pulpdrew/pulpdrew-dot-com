549f55bd63ce30e6e1c1fcb8fbca36a87888c9fd
Andrew Davis <pulpdrew@gmail.com>
2020-09-04 23:32:30 -0400

    fix css purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  purge: {
+    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+    options: {
+      whitelistPatterns: [/bg-.*-100/, /border-.*-400/],
+    }
+  },
   theme: {
     extend: {},
     fontFamily: {
